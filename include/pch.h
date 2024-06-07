991583168c63603739d33f91d92e06dde979a857
Joao-POP <joaopap@proton.me>
2024-06-07 13:58:57 -0300

    Included procompiled header

---
+++ include/pch.h
@@ -0,0 +1,24 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <SFML/Config.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#endif
