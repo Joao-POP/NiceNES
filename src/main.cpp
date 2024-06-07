d49836619df36ef728438eb3bdf0e42edbc47909
Joao-POP <joaopap@proton.me>
2024-06-07 15:30:52 -0300

    Added submodule plog

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,12 @@ int main(int argc, char **argv) {
   std::vector<sf::Keyboard::Key> p1{sf::Keyboard::J,      sf::Keyboard::K,
                                     sf::Keyboard::RShift, sf::Keyboard::Return,
                                     sf::Keyboard::W,      sf::Keyboard::S,
-                                    sf::Keyboard::A,      sf::Keyboard::D},
-      p2{sf::Keyboard::Numpad5, sf::Keyboard::Numpad6, sf::Keyboard::Numpad8,
-         sf::Keyboard::Numpad9, sf::Keyboard::Up,      sf::Keyboard::Down,
-         sf::Keyboard::Left,    sf::Keyboard::Right};
+                                    sf::Keyboard::A,      sf::Keyboard::D};
+  std::vector<sf::Keyboard::Key> p2{
+      sf::Keyboard::Numpad5, sf::Keyboard::Numpad6, sf::Keyboard::Numpad8,
+      sf::Keyboard::Numpad9, sf::Keyboard::Up,      sf::Keyboard::Down,
+      sf::Keyboard::Left,    sf::Keyboard::Right};
+
   sn::Emulator emulator;
 
   for (int i = 1; i < argc; ++i) {
